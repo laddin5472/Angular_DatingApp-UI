8f891974e4083b93709c9326108ccef5195f3689
alauddin azad <alauddin_azad2000@yahoo.com>
2021-08-31 14:02:33 -0500

    Initial works to connect Client with Web API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DatingApp-UI';
+export class AppComponent implements OnInit {
+  title = 'The Dating App';
+  users: any;
+  apiBaseUrl = 'https://localhost:44318/api/Users';
+
+  constructor(private http: HttpClient){    
+  }
+
+  ngOnInit(){
+    this.http.get(this.apiBaseUrl).subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
